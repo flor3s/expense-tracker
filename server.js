c6fe0f839d52daa609971a7d11bae60d35e9099e
flor3s <d.flores042@gmail.com>
2020-07-30 19:11:07 -0400

    prepare app for production

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const path = require('path')
 const express = require('express')
 const dotenv = require('dotenv')
 const colors = require('colors')
@@ -19,11 +20,19 @@ const app = express()
 app.use(express.json())
 
 // log http requests using morgan
-if(process.env.NODE_ENV === "development") {
+if(process.env.NODE_ENV === 'development') {
     app.use(morgan('dev'));
 }
 
 app.use('/api/v1/transactions', transactions)
+
+// make client/build our static folder and load index.html in production
+// upon GET request for anything other than api files (handled above)
+if(process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')))
+}
 
 const PORT = process.env.PORT || 5000 
 
